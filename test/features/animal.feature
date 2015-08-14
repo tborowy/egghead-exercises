7ed93616b3fc316d56254a33c0df5f0008ed7f2a
aniaw <ania_w@outlook.com>
2015-08-14 13:20:00 +0200

    add cucu

---
+++ test/features/animal.feature
@@ -0,0 +1,7 @@
+Feature: Transclusion basics
+
+  Scenario: Transclusion should be used
+    When I browse to the "/"
+    Then I should see "animal"
+
+g
---
+++ test/features/steps/steps.js
@@ -0,0 +1,28 @@
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+/*jshint -W079 */
+var expect = chai.expect;
+var fragments = require('../fragments/fragments.js');
+
+module.exports = function ()
+{
+    'use strict';
+
+    this.When(/^I browse to the "([^"]*)"$/, function (url, callback)
+    {
+        browser.get('/#' + url).then(callback);
+    });
+
+    this.Then(/^I should see "([^"]*)"$/, function (element, callback) {
+        expect(fragments(element)().isPresent()).to.become(true).and.notify(callback);
+    });
+
+    this.Then(/^pause$/, function (callback)
+    {
+        browser.pause();
+        callback();
+    });
+
+};
