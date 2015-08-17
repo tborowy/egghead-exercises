55d2d872f81bf4334b04bd6dfae5d85fc6752c7f
aniaw <ania_w@outlook.com>
2015-08-17 14:47:09 +0200

    add some tests

--- app/app.js
+++ app/app.js
@@ -4,26 +4,15 @@
 
     var app = angular.module('app', ['ngRoute']);
 
-    // complete routing
-
-    app.config(function ($routeProvider)
-    {
-
-        $routeProvider.when('/inputs/:brand?/:capacity?', {
-
-        });
-
-    });
+    // add routing
 
     app.controller('homeCtrl', function ($scope)
     {
-        $scope.$root.condition = true;
         $scope.car = {brand: 'Ferrari', capacity: 3.5};
     });
 
     app.controller('routeCtrl', function ($scope, $routeParams)
     {
-        $scope.$root.condition = true;
         $scope.brand = $routeParams.brand;
         $scope.capacity = $routeParams.capacity;
     });
--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -29,9 +29,9 @@ describe('restrict', function ()
 
         describe('"/secret"', function ()
         {
-            it('should have "routeCtrl" controller', function ()
+            it('should not have controller', function ()
             {
-                expect(route.routes['/secret'].controller).toBe('routeCtrl');
+                expect(route.routes['/secret'].controller).toBe(undefined);
 
             });
             it('should have "secret.html" view', function ()
@@ -53,6 +53,38 @@ describe('restrict', function ()
                 expect(route.routes['/details/:brand/:capacity'].templateUrl).toBe('views/details.html');
 
             });
+        });
+        describe('/inputs', function ()
+        {
+            it('should not have controller', function ()
+            {
+                expect(route.routes['/inputs/:brand?/:capacity?'].controller).toBe(undefined);
+
+            });
+
+            describe('when brand and capacity were entered', function ()
+            {
+                it('should redirect to details', function ()
+                {
+                    expect(route.routes['/inputs/:brand?/:capacity?'].redirectTo({brand: 'Ferrari', capacity: '3.5'},
+                            '/inputs/Ferrari/3.5')).toBe('/details/Ferrari/3.5');
+
+                });
+
+                it('should redirect to secret when brand is "special"', function ()
+                {
+                    expect(route.routes['/inputs/:brand?/:capacity?'].redirectTo({brand: 'special', capacity: '3.5'}, '/inputs/special/3.5')).toBe('/secret');
+
+                });
+                it('should redirect to secret when capacity is "special"', function ()
+                {
+                    expect(route.routes['/inputs/:brand?/:capacity?'].redirectTo({brand: 'Ferrari', capacity: 'special'},
+                            '/inputs/Ferrari/special')).toBe('/secret');
+
+                });
+
+            });
+
         });
 
         describe('otherwise', function ()
