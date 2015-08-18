ee2b9ce4e4d2fcdc9fc6ae87ab1a501f1242bf9b
aniaw <ania_w@outlook.com>
2015-08-18 09:50:10 +0200

    test ok

--- app/song.js
+++ app/song.js
@@ -4,20 +4,16 @@ var songApp = angular.module('songApp', []);
 
 songApp.factory('Song', function ()
 {
-    songApp.factory('Song', function ()
-    {
-        return {
-            author: 'Gotye feat. Kimbra',
-            title: 'Somebody That I Used To Know',
-            sing: 'But you did not have to cut me off... But you treat me like a stranger ...',
-            fake: 'Buuuuuuuuuuuuuut you niah niah blah bla ...'
-        };
-    });
+    return {
+        author: 'Gotye feat. Kimbra',
+        title: 'Somebody That I Used To Know',
+        sing: 'But you did not have to cut me off... But you treat me like a stranger ...',
+        fake: 'Buuuuuuuuuuuuuut you niah niah blah bla ...'
+    };
 });
 
 songApp.controller('SongCtrl', function (Song)
 {
-
     var ctrl = this;
     ctrl.showSongText = function ()
     {
@@ -30,6 +26,7 @@ songApp.controller('SongCtrl', function (Song)
         ctrl.showSing = false;
         ctrl.showFake = true;
     };
+
 
     ctrl.author = Song.author;
     ctrl.title = Song.title;
