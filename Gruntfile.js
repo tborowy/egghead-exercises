151958ad4d5bf48b2071e1a21f9169552a1bdb60
aniaw <ania_w@outlook.com>
2015-08-18 13:31:24 +0200

    update test conf

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,7 @@ module.exports = function (grunt)
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-karma');
 
     require('load-grunt-tasks')(grunt);
@@ -21,38 +22,28 @@ module.exports = function (grunt)
     };
 
     grunt.initConfig({
-        config: config,
-        watch: {
+        config: config, watch: {
             livereload: {
                 options: {
                     livereload: '<%= connect.options.livereload %>'
-                },
-                files: [
-                    '<%= config.app %>/**/*.html', '<%= config.app %>/**/*.js'
-                ]
+                }, files: ['<%= config.app %>/**/*.html', '<%= config.app %>/**/*.js']
             }
-        },
-        connect: {
+        }, connect: {
             options: {
-                port: 9000,
-                livereload: 35729,
-                hostname: 'localhost'
-            },
-            livereload: {
+                port: 9000, livereload: 35729, hostname: 'localhost'
+            }, livereload: {
                 options: {
-                    open: true,
-                    middleware: function (connect)
+                    open: true, middleware: function (connect)
                     {
-                        return [
-                            connect().use('/bower_components', connect.static('./bower_components')), connect.static(config.app)
+                        return [connect().use('/bower_components', connect.static('./bower_components')), connect.static(config.app)
 
                         ];
                     }
                 }
             }
-        },
-
-        karma: {
+        }, jshint: {
+            all: ['app/**/*.js']
+        }, karma: {
             unit: {
                 configFile: 'test/karma.conf.js'
             }
@@ -61,9 +52,7 @@ module.exports = function (grunt)
 
     grunt.registerTask('serve', function ()
     {
-        grunt.task.run([
-            'connect:livereload', 'watch'
-        ]);
+        grunt.task.run(['connect:livereload', 'watch']);
     });
 
     grunt.registerTask('default', ['serve']);
