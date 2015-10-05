8ad307e425babf80a9c528b7e60c995b9fd341a9
aniaw <ania_w@outlook.com>
2015-10-05 12:24:52 +0200

    fix selectors, tests & README

--- app/filter.js
+++ app/filter.js
@@ -5,14 +5,14 @@
 
 //add quote filter
 //add withoutH filter
-//add firsLetterUp filter
+//add firstLetterUp filter
 
     app.controller('FilterCtrl', function ($scope)
     {
         $scope.data = {
             citation: 'To be, or not to be...',
             title: 'The Hobbit: The Battle of the Five Armies',
-            someText: 'Visit BBC News for up-to-the-minute news, breaking news, video, audio and stories.'
+            someText: 'Visit News for up-to-the-minute news, breaking news, video, audio and stories.'
         };
     });
 })();
